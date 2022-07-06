ca05af0ee69d4ed10fdcfda695970be209fe5f64
Boi Schrader <boi1992@t-online.de>
2022-07-06 15:50:09 +0200

    done

--- solution.js
+++ solution.js
@@ -1,1 +1,22 @@
-// write your code in this file
+
+// Basic arithmetic problems
+
+var ticket = 15;
+var income = 6450;
+var visitors ;
+visitors = income / ticket
+console.log('------------------------------------------')
+console.log('In one night we had' ,visitors,'visitors!')
+
+
+var wsalary = 500;
+var months= 12;
+var weeks = 4;
+var ysalary;
+ysalary = (wsalary * weeks) * months
+console.log('------------------------------------------')
+console.log('Sylvia makes', ysalary, 'a year.')
+
+// Percentage
+
+console.log(17/30)
